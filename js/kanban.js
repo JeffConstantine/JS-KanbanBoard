205570decc8792ab47032d0f8e75d2732fb2d8e3
Jeff Constantine <conno80@gmail.com>
2022-02-03 11:36:43 +0800

    Update kanban.js
    fixed const id syntax

--- js/kanban.js
+++ js/kanban.js
@@ -49,7 +49,7 @@ const create_item = () => {
 document.querySelectorAll(".drop").forEach((element) => {
   element.addEventListener("drop", (event) => {
     event.preventDefault();
-    event.dataTransfer.getData('text', event.target.id);
+    const id = event.dataTransfer.getData('text');
 
     
   });
