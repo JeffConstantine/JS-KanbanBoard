f3bddabb499e0f85c812ded4967c012f69766fc5
Jeff Constantine <conno80@gmail.com>
2022-02-03 11:04:52 +0800

    Update kanban.js
    fixed syntax on let order

--- js/kanban.js
+++ js/kanban.js
@@ -32,7 +32,7 @@ const create_item = () => {
   save_btn.addEventListener('click', () => {
     error.innerHTML = '';
     if (input.value !== '') {
-      let order = order +1;
+      let order += order;
       item.innerHTML = input.value;
       adding = false;
     }
