084f9f33e37d4d27ad7038f2ff6d320027e8ec6c
Jeff Constantine <conno80@gmail.com>
2022-02-03 10:13:32 +0800

    Update kanban.js
    added item classList and gave it an id

--- js/kanban.js
+++ js/kanban.js
@@ -17,6 +17,9 @@ add_btn.addEventListener('click', () => {
 
 const create_item = () => {
   let item = document.createElement('div');
+  item.classList.add('item');
+  item.id = 'item-' + order;
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
