4d70f63dd6183d6ff4ee040d3e051136f7fe5b45
Jeff Constantine <conno80@gmail.com>
2022-02-03 11:07:34 +0800

    Update kanban.js
    changed + sign order

--- js/kanban.js
+++ js/kanban.js
@@ -32,7 +32,7 @@ const create_item = () => {
   save_btn.addEventListener('click', () => {
     error.innerHTML = '';
     if (input.value !== '') {
-      order =+ 1;
+      order += 1;
       item.innerHTML = input.value;
       adding = false;
     }
